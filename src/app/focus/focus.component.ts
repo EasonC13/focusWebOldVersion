2f099baabd817600dfd12048673f6f2565421fe5
EasonC13 <gtes9950406@gmail.com>
2020-10-25 16:44:41 +0800

    2020/10/25 (add) try mobilenet at focus component

--- src/app/focus/focus.component.ts
+++ src/app/focus/focus.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as tf from "@tensorflow/tfjs";
+import * as mobilenet from '@tensorflow-models/mobilenet';
 
 @Component({
   selector: 'app-focus',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./focus.component.scss']
 })
 export class FocusComponent implements OnInit {
+  model: any;
+  loading: boolean;
+  imgSrc: any;
+  predictions:any;
 
   constructor() { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.loading = true;
+    this.model = await mobilenet.load();
+    this.loading = false;
   }
+
+  async fileChange(event) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+
+      reader.readAsDataURL(file);
+
+      reader.onload = (res: any) => {
+        this.imgSrc = res.target.result;
+        this.predictions = this.model.classify(this.imgSrc)
+      };
+    }
+  }
+  
 
 }
